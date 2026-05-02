87104e6c286e638f3711e6ae15d4abc6ce415c8b
Luke Salem <lukehsa>
2026-05-01 21:30:40 -0400

    Complete ISSUE-9: Add StudentService singleton

---
+++ LMS/CLI.LMS/Services/StudentService.cs
@@ -0,0 +1,37 @@
+using CLI.LMS.Models;
+
+namespace CLI.LMS.Services
+{
+    public class StudentService
+    {
+        private static StudentService _instance;
+        public static StudentService Current
+        {
+            get
+            {
+                if (_instance == null) _instance = new StudentService();
+                return _instance;
+            }
+        }
+        private StudentService() { }
+
+        private List<Student> _students = new List<Student>();
+
+        public List<Student> GetAll() => _students;
+
+        public Student GetById(int id) => _students.FirstOrDefault(s => s.Id == id);
+
+        public Student Add(string name, string code, string classification)
+        {
+            var student = new Student
+            {
+                Id = _students.Count > 0 ? _students.Max(s => s.Id) + 1 : 1,
+                Name = name,
+                Code = code,
+                Classification = classification
+            };
+            _students.Add(student);
+            return student;
+        }
+    }
+}

390587f7f16dc6ffaac8a64980e67617975cd060
Luke Salem <lukehsa>
2026-05-01 21:30:27 -0400

    Complete ISSUE-9: Create Submission model

---
+++ LMS/CLI.LMS/Models/Submission.cs
@@ -0,0 +1,12 @@
+namespace CLI.LMS.Models
+{
+    public class Submission
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int AssignmentId { get; set; }
+        public string Content { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public int? PointsAwarded { get; set; }
+    }
+}

0c833391c3e52d6ad13f74594e87408bd2f2fefc
Luke Salem <lukehsa>
2026-05-01 21:30:00 -0400

    Complete ISSUE-9: Create User model

---
+++ LMS/CLI.LMS/Models/User.cs
@@ -0,0 +1,9 @@
+namespace CLI.LMS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
